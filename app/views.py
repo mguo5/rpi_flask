4ccbebcf7565e9f93f069b83e2ee34470ac7841c
mguo5 <mattguo5@gmail.com>
2022-01-30 12:38:32 -0800

    used static folders to display breadboard

--- app/views.py
+++ app/views.py
@@ -42,7 +42,9 @@ def blue_on():
     uart.ser.reset_input_buffer()
     to_send = "LBO"
     uart.send_thru_uart(to_send)
-    return render_template('index.html')
+    print("Blue_on")
+    return ("nothing")
+    #return render_template('index.html')
 
 @view.route('/blue_off')
 def blue_off():
@@ -53,7 +55,9 @@ def blue_off():
     uart.ser.reset_input_buffer()
     to_send = "LBF"
     uart.send_thru_uart(to_send)
-    return render_template('index.html')
+    print("Blue_off")
+    return ("nothing")
+    #return render_template('index.html')
 
 @view.route('/red_on')
 def red_on():
@@ -64,7 +68,9 @@ def red_on():
     uart.ser.reset_input_buffer()
     to_send = "LRO"
     uart.send_thru_uart(to_send)
-    return render_template('index.html')
+    print("Red_on")
+    return ("nothing")
+    #return render_template('index.html')
 
 @view.route('/red_off')
 def red_off():
@@ -75,4 +81,136 @@ def red_off():
     uart.ser.reset_input_buffer()
     to_send = "LRF"
     uart.send_thru_uart(to_send)
-    return render_template('index.html')
+    print("Red_off")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/orange_on')
+def orange_on():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "LOO"
+    uart.send_thru_uart(to_send)
+    print("Orange_on")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/orange_off')
+def orange_off():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "LOF"
+    uart.send_thru_uart(to_send)
+    print("Orange_off")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/green_on')
+def green_on():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "LGO"
+    uart.send_thru_uart(to_send)
+    print("Green_on")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/green_off')
+def green_off():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "LGF"
+    uart.send_thru_uart(to_send)
+    print("Green_off")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/porta_on')
+def porta_on():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "GAO"
+    uart.send_thru_uart(to_send)
+    print("Porta_on")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/porta_off')
+def porta_off():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "GAF"
+    uart.send_thru_uart(to_send)
+    print("Porta_off")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/portb_on')
+def portb_on():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "GBO"
+    uart.send_thru_uart(to_send)
+    print("Portb_on")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/portb_off')
+def portb_off():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "GBF"
+    uart.send_thru_uart(to_send)
+    print("Portb_off")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/portc_on')
+def portc_on():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "GCO"
+    uart.send_thru_uart(to_send)
+    print("Portc_on")
+    return ("nothing")
+    #return render_template('index.html')
+    
+@view.route('/portc_off')
+def portc_off():
+    """Route to turn off the Red LED. TODO: put functionality as a button onClick method to avoid reloading page"""
+    uart = uart_communicate()
+    if(uart.ser.isOpen() == False):
+        uart.ser.open()
+    uart.ser.reset_input_buffer()
+    to_send = "GCF"
+    uart.send_thru_uart(to_send)
+    print("Portc_off")
+    return ("nothing")
+    #return render_template('index.html')
